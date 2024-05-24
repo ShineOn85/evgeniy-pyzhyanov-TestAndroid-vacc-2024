d83e0bd3d7b6b2562933478edaeddfeb23fd231a
AbsoluteLee <shineon85@yandex.ru>
2024-05-24 13:23:29 +0500

    add onLoginClickListener

--- app/src/main/java/ru/absolutelee/fakestoreapp/presentation/main/MainActivity.kt
+++ app/src/main/java/ru/absolutelee/fakestoreapp/presentation/main/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FakeStoreAppTheme(dynamicColor = false) {
-                MainScreen()
+
             }
         }
     }
